c9b9c378efcb578c1e11c31a09f2f1f5ac66d06f
brois <brois8572@gmail.com>
2023-01-09 08:47:21 +0100

    git init

---
+++ src/main/kotlin/com/example/projetwebserv/ServletInitializer.kt
@@ -0,0 +1,12 @@
+package com.example.projetwebserv
+
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
+class ServletInitializer : SpringBootServletInitializer() {
+
+    override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+        return application.sources(ProjetWebServApplication::class.java)
+    }
+
+}
---
+++ src/test/kotlin/com/example/projetwebserv/ProjetWebServApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.projetwebserv
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProjetWebServApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
