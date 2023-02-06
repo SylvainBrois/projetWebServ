ee317f3083da7e05a89082fd40ab496e8b374747
NicolasB <nicolas.bervoas@etu.univ-nantes.fr>
2023-02-06 11:42:28 +0100

    refacto : Beer -> removed unused imports

--- src/main/kotlin/com/example/projetwebserv/features/beer/dao/entity/CreateBeer.kt
+++ src/main/kotlin/com/example/projetwebserv/features/beer/dao/entity/CreateBeer.kt
@@ -1,10 +1,5 @@
 package com.example.projetwebserv.features.beer.dao.entity
 
-import com.example.projetwebserv.features.brewery.dao.entity.Brewery
-import jakarta.persistence.FetchType
-import jakarta.persistence.JoinColumn
-import jakarta.persistence.ManyToOne
-
 data class CreateBeer(val abv : Float,
                       val ibu : Float?,
                       val name : String,
--- src/main/kotlin/com/example/projetwebserv/features/beer/dao/repository/BeerRepository.kt
+++ src/main/kotlin/com/example/projetwebserv/features/beer/dao/repository/BeerRepository.kt
@@ -1,7 +1,6 @@
 package com.example.projetwebserv.features.brewery.dao.repository
 
 import com.example.projetwebserv.features.beer.dao.entity.Beer
-import com.example.projetwebserv.features.brewery.dao.entity.Brewery
 import org.springframework.data.repository.CrudRepository
 
 interface BeerRepository : CrudRepository<Beer, Long> {
